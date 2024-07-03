3bc45a5759f1994adc9d9d4b448c33cfdcf9b836
Daniel Vigano <unb10@toledobrasil.com>
2024-07-03 11:18:11 -0300

    Primeira versão, com mf_videos inicial interagindo com API do youtube

---
+++ bff/server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {foo: 'FOO'});
+});
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
---
+++ mf_videos/server.js
@@ -0,0 +1,36 @@
+let express = require('express');
+const axios = require("axios");
+let app = express();
+
+require('dotenv').config({ path: '../.env' });
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index',{ videos: [] });
+});
+
+app.post('/api/search', async (req, res) => {
+    const query = await req.body.searchInput;
+    
+    try {
+        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+                part: 'snippet',
+                q: query,
+                key: process.env.API_KEY_YOUTUBE,
+                type: 'video',
+                maxResults : 12,
+            }
+        });
+        res.render("index", { videos: response.data.items });
+        // res.json(response.data.items);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+app.listen(4000, () => console.log('Listening on port 4000!'));
